60b8879d3e8edc766a3d8588e6656e09d636b92e
Elijah Jose Arcedera <eli.arcedera@gmail.com>
2022-05-17 01:37:21 +0800

    Creates reviews component

---
+++ src/components/reviews/Review.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import styles from "./Styles";
+
+const Review = ({ text, color, icon }) => {
+    console.log('Review: ', text, color, typeof icon);
+    return (
+        <View style={{
+            backgroundColor: `${color}`,
+            borderRadius: 30,
+            marginTop: 15,
+            height: 50,
+            width: 300,
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingHorizontal: 20,
+            flexDirection: 'row',
+        }}>
+            <View style={styles.iconStyle}>
+                {typeof icon === 'number' ? <Image source={icon} style={{ width: 30, height: 30 }} /> : icon}
+            </View>
+            <Text style={styles.textStyles}>{text}</Text>
+        </View >
+    );
+}
+
+export default Review;
---
+++ src/components/reviews/Styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: 'white',
+        borderRadius: 30,
+        height: 50,
+        width: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+    },
+    textStyles: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 12,
+        marginLeft: 10,
+    },
+    iconStyle: {
+        borderRadius: 30,
+        overflow: 'hidden',
+    }
+});
+
+export default styles;
