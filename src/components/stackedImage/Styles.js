1e82cb0ec0baae98e4ea4d2f0342c4c89fc3f044
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2022-05-21 17:19:43 +0800

    Creates styles file for StackedImage

---
+++ src/components/stackedImage/Styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        flexDirection: 'row',
+        marginTop: 25,
+        marginBottom: 35
+    },
+    imageStyle: {
+        borderRadius: 50,
+        overflow: 'hidden',
+        margin: -3
+    }
+});
+
+export default styles;
