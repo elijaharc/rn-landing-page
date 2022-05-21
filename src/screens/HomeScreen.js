2dd3664a0a8d2dc140e8c0ca57a2cf0c52aec5d3
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2022-05-21 17:20:30 +0800

    Removes unnecessary props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,36 +1,26 @@
-import React, { useState } from "react";
-import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import { SafeAreaView, ScrollView } from "react-native";
 import Navbar from "../components/navbar/Navbar";
 import Hero from "../components/hero/Hero";
 import Mission from "../components/mission/Mission";
 import Footer from "../components/footer/Footer";
-import i18n from 'i18n-js';
 import { en, ph } from '../../i18n/supportedLanguages';
+import i18n from 'i18n-js';
 i18n.fallbacks = true;
 i18n.translations = { en, ph };
 
 const HomeScreen = ({ navigation }) => {
-    const [locale, setLocale] = useState("en");
-
-    i18n.locale = locale;
-
     return (
         <SafeAreaView>
             <Navbar
-                navigation={navigation}
-                locale={locale}
-                setLocale={setLocale} />
+                navigation={navigation} />
             <ScrollView>
-                <Hero navigation={navigation}
-                    locale={locale} />
-                <Mission navigation={navigation}
-                    locale={locale} />
-                <Footer locale={locale} />
+                <Hero navigation={navigation} />
+                <Mission navigation={navigation} />
+                <Footer />
             </ScrollView>
         </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({});
 
 export default HomeScreen;
